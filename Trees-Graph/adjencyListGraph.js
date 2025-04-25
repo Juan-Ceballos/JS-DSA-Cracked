a4a3b1958cedc6b955ac7177534b7e023a53a029
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2025-04-24 22:16:50 -0400

    graph adjacency list

---
+++ Trees-Graph/adjencyListGraph.js
@@ -0,0 +1,12 @@
+class Graph {
+    constructor(nodes) {
+        this.nodes = nodes
+    }
+}
+
+class Node {
+    constructor(name, children) {
+        this.name = name
+        this.children = children
+    }
+}
